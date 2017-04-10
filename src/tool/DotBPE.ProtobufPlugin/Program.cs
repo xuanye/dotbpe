4fe1ec92e098877bd88d0e9aeb6b36b7ff6478a8
假正经哥哥 <xuanye.wan@gmail.com>
2017-04-10 13:36:24 +0800

    初始化代码生成类

---
+++ src/tool/DotBPE.ProtobufPlugin/Program.cs
@@ -0,0 +1,52 @@
+﻿using Google.Protobuf.Compiler;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Google.Protobuf;
+
+namespace DotBPE.ProtobufPlugin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CodeGeneratorRequest request;
+            var response = new CodeGeneratorResponse();
+            try
+            {
+                using (var inStream = Console.OpenStandardInput())
+                {
+                    request = CodeGeneratorRequest.Parser.ParseFrom(inStream);
+                }
+                ParseCode(request, response);
+            }
+            catch (Exception e)
+            {
+                response.Error += e.ToString();
+            }
+
+            using (var output = Console.OpenStandardOutput())
+            {                
+                //byte[] bytea= response.ToByteArray();
+                //response.Error += Encoding.UTF8.GetString(bytea);
+                response.WriteTo(output);
+                output.Flush();
+
+            }          
+        }
+        private static void ParseCode(CodeGeneratorRequest request, CodeGeneratorResponse response)
+        {
+            List<string> slist = new List<string>();
+            foreach( var protofile in request.ProtoFile)
+            {
+                var nfile = new CodeGeneratorResponse.Types.File();
+                nfile.Name = protofile.Name + ".json";
+                nfile.Content = "{value:\"test\"}";
+                response.File.Add(nfile);
+            }
+        }
+    }
+}
