6caa4b4c577b4d4403f341a03fa4f14af7927237
假正经哥哥 <xuanye.wan@gmail.com>
2017-04-27 19:42:55 +0800

    修复tools没有上传的问题

--- scripts/publish/publish_benchmark.sh
+++ scripts/publish/publish_benchmark.sh
@@ -2,7 +2,12 @@ set -ex
 
 cd $(dirname $0)/../../src/test/IntegrationTesting/
 
-BUILD_DIR=../../../../artifacts/IntegrationTesting/
+BUILD_DIR=../../../artifacts/IntegrationTesting
 
-dotnet publish ./DotBPE.IntegrationTesting.Client/DotBPE.IntegrationTesting.Client.csproj -o $BUILD_DIR/client -c Release
-dotnet publish ./DotBPE.IntegrationTesting.QpsServer/DotBPE.IntegrationTesting.QpsServer.csproj -o $BUILD_DIR/qpsserver -c Release
+if [ -d $BUILD_DIR ]; then
+  rm -R $BUILD_DIR
+fi
+
+
+dotnet publish ./DotBPE.IntegrationTesting.Client/DotBPE.IntegrationTesting.Client.csproj -o ../$BUILD_DIR/client -c Release
+dotnet publish ./DotBPE.IntegrationTesting.QpsServer/DotBPE.IntegrationTesting.QpsServer.csproj -o ../$BUILD_DIR/qpsserver -c Release
--- scripts/test/run_client.sh
+++ scripts/test/run_client.sh
@@ -2,4 +2,4 @@ set -ex
 
 cd $(dirname $0)/../../artifacts/IntegrationTesting
 
-dotnet ./client/DotBPE.IntegrationTesting.Client.dll --server 127.0.0.1:6201 --testcase benchmark --rtc 5  --rc 10000 --mpc 8
+dotnet ./client/DotBPE.IntegrationTesting.Client.dll --server 127.0.0.1:6201 --testcase benchmark --rtc 8  --rc 50000 --mpc 8
