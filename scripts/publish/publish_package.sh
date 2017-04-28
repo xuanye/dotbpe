e76c0b634cd78f0a44da3a9eb1d0c7c7cfe54da0
假正经哥哥 <xuanye.wan@gmail.com>
2017-04-28 11:05:08 +0800

    添加发布的代码

---
+++ scripts/publish/publish_package.sh
@@ -0,0 +1,42 @@
+set -ex
+
+cd $(dirname $0)/../src/
+
+artifactsFolder="../artifacts"
+
+if [ -d $artifactsFolder ]; then
+  rm -R $artifactsFolder
+fi
+
+mkdir -p $artifactsFolder
+
+dotnet restore ./DotBPE.sln
+dotnet build ./DotBPE.sln -c Release
+
+
+revision="${TRAVIS_BUILD_NUMBER:=1}"
+versionNumber="0.1.${revision}-alpha"
+
+
+
+dotnet test ./test/DotBPE.UnitTest/DotBPE.UnitTest.csproj
+
+if [ "$TRAVIS_BRANCH" == "master" ]; then
+
+    dotnet pack ./core/DotBPE.Rpc/DotBPE.Rpc.csproj -c Release -o ../$artifactsFolder --version-suffix=$versionNumber
+    dotnet pack ./core/DotBPE.Rpc.Netty/DotBPE.Rpc.Netty.csproj -c Release -o ../$artifactsFolder --version-suffix=$versionNumber
+    dotnet pack ./core/DotBPE.Plugin/DotBPE.Plugin.csproj -c Release -o ../$artifactsFolder --version-suffix=$versionNumber
+    dotnet pack ./protocol/DotBPE.Protocol.Amp/DotBPE.Protocol.Amp.csproj -c Release -o ../$artifactsFolder --version-suffix=$versionNumber
+
+
+
+    dotnet nuget push ./$artifactsFolder/DotBPE.Rpc.${versionNumber}.nupkg -k $NUGET_KEY -s https://www.nuget.org
+    dotnet nuget push ./$artifactsFolder/DotBPE.Rpc.Netty.${versionNumber}.nupkg -k $NUGET_KEY -s https://www.nuget.org
+    dotnet nuget push ./$artifactsFolder/DotBPE.Plugin.${versionNumber}.nupkg -k $NUGET_KEY -s https://www.nuget.org
+    dotnet nuget push ./$artifactsFolder/DotBPE.Protocol.Amp.${versionNumber}.nupkg -k $NUGET_KEY -s https://www.nuget.org
+
+
+    nuget pack ./DotBPE.nuspec  -Version $versionNumber -OutputDirectory ../$artifactsFolder
+    dotnet nuget push ./$artifactsFolder/DotBPE.${versionNumber}.nupkg -k $NUGET_KEY -s https://www.nuget.org
+
+fi
