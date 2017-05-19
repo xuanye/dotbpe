bf6a3b84c55c1ee07f0e40e9ef3b9269fc8c71e4
假正经哥哥 <xuanye.wan@gmail.com>
2017-05-19 15:46:18 +0800

    删除了不该删除的问题件

---
+++ src/protocol/DotBPE.Protocol.Amp/NotFoundServiceActor.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using DotBPE.Rpc;
+using DotBPE.Rpc.Codes;
+
+namespace DotBPE.Protocol.Amp
+{
+    public class NotFoundServiceActor: IServiceActor<AmpMessage>
+    {
+        public static NotFoundServiceActor Default  = new NotFoundServiceActor();
+
+        public string Id
+        {
+            get { return "NotFoundServiceActor"; }
+        }
+
+        public async Task ReceiveAsync(IRpcContext<AmpMessage> context, AmpMessage message)
+        {
+            AmpMessage response = new AmpMessage
+            {
+                InvokeMessageType = InvokeMessageType.NotFound,
+                ServiceId = message.ServiceId,
+                MessageId = message.MessageId
+            };
+            response.Sequence = message.Sequence;
+            await context.SendAsync(response);
+        }
+    }
+}
