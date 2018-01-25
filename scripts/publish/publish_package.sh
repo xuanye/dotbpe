52fa84579148ffed0a5292f66ed8e9af2e7cbf88
假正经哥哥 <xuanye.wan@gmail.com>
2018-01-25 17:39:37 +0800

    升级一下版本号

--- scripts/publish/publish_package.sh
+++ scripts/publish/publish_package.sh
@@ -15,7 +15,7 @@ dotnet build ./DotBPE.sln -c Release
 
 
 revision="${TRAVIS_BUILD_NUMBER:=1}"
-versionNumber="1.0.${revision}"
+versionNumber="1.1.${revision}"
 
 
 
@@ -26,13 +26,13 @@ if [ "$TRAVIS_BRANCH" == "master" ]; then
     dotnet pack ./core/DotBPE.Rpc/DotBPE.Rpc.csproj -c Release -o ../../$artifactsFolder --version-suffix=$versionNumber
     dotnet pack ./core/DotBPE.Rpc.Netty/DotBPE.Rpc.Netty.csproj -c Release -o ../../$artifactsFolder --version-suffix=$versionNumber
     dotnet pack ./protocol/DotBPE.Protocol.Amp/DotBPE.Protocol.Amp.csproj -c Release -o ../../$artifactsFolder --version-suffix=$versionNumber
-
+    dotnet pack ./protocol/DotBPE.Protobuf/DotBPE.Protobuf.csproj -c Release -o ../../$artifactsFolder --version-suffix=$versionNumber
 
 
     dotnet nuget push ./$artifactsFolder/DotBPE.Rpc.${versionNumber}.nupkg -k $NUGET_KEY -s https://www.nuget.org
     dotnet nuget push ./$artifactsFolder/DotBPE.Rpc.Netty.${versionNumber}.nupkg -k $NUGET_KEY -s https://www.nuget.org
     dotnet nuget push ./$artifactsFolder/DotBPE.Protocol.Amp.${versionNumber}.nupkg -k $NUGET_KEY -s https://www.nuget.org
-
+    dotnet nuget push ./$artifactsFolder/DotBPE.Protobuf.${versionNumber}.nupkg -k $NUGET_KEY -s https://www.nuget.org
 
     #nuget pack ./DotBPE.nuspec  -Version $versionNumber -OutputDirectory ../$artifactsFolder
     #dotnet nuget push ./$artifactsFolder/DotBPE.${versionNumber}.nupkg -k $NUGET_KEY -s https://www.nuget.org
