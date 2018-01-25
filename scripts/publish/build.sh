66069a5f044d3e3cb9d8ea8280e8d73e4b306b29
假正经哥哥 <xuanye.wan@gmail.com>
2018-01-25 18:44:51 +0800

    移除了master push 自动打包的功能

---
+++ scripts/publish/build.sh
@@ -0,0 +1,14 @@
+set -ex
+
+cd $(dirname $0)/../../src/
+
+artifactsFolder="../artifacts"
+
+if [ -d $artifactsFolder ]; then
+  rm -R $artifactsFolder
+fi
+
+mkdir -p $artifactsFolder
+
+dotnet restore ./DotBPE.sln
+dotnet build ./DotBPE.sln -c Release
