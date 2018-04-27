4a1d433dd9b001990c60633c2fced3bd29d30593
假正经哥哥 <xuanye.wan@gmail.com>
2018-04-27 08:50:12 +0800

    升级版本

--- scripts/publish/publish_package.sh
+++ scripts/publish/publish_package.sh
@@ -15,7 +15,7 @@ dotnet build ./DotBPE.sln -c Release
 
 
 
-versionNumber="1.1.25"
+versionNumber="1.1.27"
 
 dotnet pack ./core/DotBPE.Rpc.Abstractions/DotBPE.Rpc.Abstractions.csproj -c Release -o ../../$artifactsFolder --version-suffix=$versionNumber
 dotnet pack ./core/DotBPE.Rpc/DotBPE.Rpc.csproj -c Release -o ../../$artifactsFolder --version-suffix=$versionNumber
