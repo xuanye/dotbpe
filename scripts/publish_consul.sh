7ba1a72182847eb3b97dd96554b761bc2ffc0743
Xuanye Wong <xuanye.wan@gmail.com>
2019-01-30 16:43:18 +0800

    为发布做准备

---
+++ scripts/publish_consul.sh
@@ -0,0 +1,16 @@
+set -ex
+
+cd $(dirname $0)/../
+
+artifactsFolder="./artifacts"
+
+if [ -d $artifactsFolder ]; then
+  rm -R $artifactsFolder
+fi
+
+mkdir -p $artifactsFolder
+
+
+dotnet pack ./src/DotBPE.Extra.Consul/DotBPE.Extra.Consul.csproj -c Release -o ../../$artifactsFolder
+
+dotnet nuget push ./$artifactsFolder/DotBPE.Extra.Consul.*.nupkg -k $NUGET_KEY -s https://www.nuget.org
---
+++ scripts/publish_rpc.sh
@@ -0,0 +1,16 @@
+set -ex
+
+cd $(dirname $0)/../
+
+artifactsFolder="./artifacts"
+
+if [ -d $artifactsFolder ]; then
+  rm -R $artifactsFolder
+fi
+
+mkdir -p $artifactsFolder
+
+
+dotnet pack ./src/DotBPE.Rpc/DotBPE.Rpc.csproj -c Release -o ../../$artifactsFolder
+
+dotnet nuget push ./$artifactsFolder/DotBPE.Rpc.*.nupkg -k $NUGET_KEY -s https://www.nuget.org
